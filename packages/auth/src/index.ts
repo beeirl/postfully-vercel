a067732e4578e73130468efe9bd87d43c2609097
BEEIRL <c@beeirl.com>
2025-09-06 11:46:48 +0200

    sync

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 
 const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  .get('/', (c) => {
+    return c.text('Hello Hono!')
+  })
+  .get('/test', (c) => {
+    return c.text('Test')
+  })
 
 export default app
