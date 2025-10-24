c9881a6cea9d4504290c51300f683fa2e95a2318
BEEIRL <c@beeirl.com>
2025-10-24 19:46:58 +0200

    sync

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,10 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: [
-    'prettier-plugin-astro',
     'prettier-plugin-organize-imports',
     'prettier-plugin-tailwindcss',
   ],
   printWidth: 100,
   semi: false,
   singleQuote: true,
-  trailingComma: 'es5',
 }
